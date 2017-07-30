0f86c5a5848e7f832ba5fd638a6a2a14d3c3ddd5
Stephan Rayner <stephan.rayner@gmail.com>
2017-07-30 11:27:18 -0700

    Made utils deleted unused things and made Ip it's own module

--- plugins/Elephant/BabyElephants.py
+++ plugins/Elephant/BabyElephants.py
@@ -1,14 +1,11 @@
-import sys
-sys.path.append("../lib/")
-
 from slackbot.bot import respond_to
-from lib.RandomRedditRetriever import RandomRedditRetriever
+from utils.RandomRedditRetriever import RandomRedditRetriever
 from random import random
 import re
 
-@respond_to('cheese', re.IGNORECASE)
+@respond_to('elephant me', re.IGNORECASE)
 def elephant_me(message):
     print("elephant plugin hit")
-    url = "http://www.reddit.com/r/babyelephants.json"
+    url = "http://www.reddit.com/r/dogs.json"
     randomRedditRetriever = RandomRedditRetriever(url)
     message.reply(randomRedditRetriever.getRandomUrl())
---
+++ plugins/__init__.py
@@ -0,0 +1,5 @@
+import os
+from sys import path
+
+root = os.path.abspath(os.pardir)
+path.append(root)
