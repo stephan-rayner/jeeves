7e5fdfd6eb4f8699dbbe3225e760171dbf9f1384
Stephan Rayner <stephan.rayner@gmail.com>
2017-07-30 11:29:00 -0700

    Making the elephant plugin hit /r/babyelephants

--- plugins/Elephant/BabyElephants.py
+++ plugins/Elephant/BabyElephants.py
@@ -6,6 +6,6 @@ import re
 @respond_to('elephant me', re.IGNORECASE)
 def elephant_me(message):
     print("elephant plugin hit")
-    url = "http://www.reddit.com/r/dogs.json"
+    url = "http://www.reddit.com/r/babyelephants.json"
     randomRedditRetriever = RandomRedditRetriever(url)
     message.reply(randomRedditRetriever.getRandomUrl())
