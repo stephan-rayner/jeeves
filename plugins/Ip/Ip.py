87ae10bb42262c4ce56f3e5b7f7273c962925bc8
Stephan Rayner <stephan.rayner@gmail.com>
2017-11-05 23:06:52 -0800

    WIP: Trying to get the ip pluging to work
    Thus far no join :(

--- plugins/Ip/Ip.py
+++ plugins/Ip/Ip.py
@@ -1,14 +1,11 @@
 from slackbot.bot import respond_to
 from slackbot.bot import listen_to
-import socket
+import os
 import re
 
-from subprocess import call
-call(["ls", "-l"])
+# regex = re.compile(r'\bipconfig\b', re.IGNORECASE)
 
-regex = re.compile(r'\bWhat is your ip?\b', re.MULTILINE | re.IGNORECASE)
-
-@respond_to(regex)
-def ping(message):
+@respond_to("ipconfig")
+def find_ip(message):
     ip = os.popen("ifconfig | grep '192.168'").read().split(" ")[1]
     message.reply('My Ip is {}'.format(ip))
